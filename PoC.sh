2e6086f052446e675f2c119fe993e50d8ebb00db
JacksonGL <jacksongl1988@gmail.com>
2017-04-17 22:53:47 -0700

    one more vulnerable package

--- PoC.sh
+++ PoC.sh
@@ -17,6 +17,9 @@ elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     exit
 fi
 
+echo -e "\nDirectory traversal: earlybird"
+./directory-traversal/earlybird/PoC.sh
+
 echo -e "\nDirectory traversal: guaycuru"
 ./directory-traversal/guaycuru/PoC.sh
 
